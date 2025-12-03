3abc2980dacd12e99709c8f061e8a3179e03ff6b
Xgor <real.xgor@hotmail.com>
2025-12-03 17:46:56 +0100

    Added Junkyard Area

--- scripts/locations.lua
+++ scripts/locations.lua
@@ -2,3 +2,4 @@ Tracker:AddLocations("locations/worldmap.jsonc")
 
 Tracker:AddLocations("locations/south/startcamp.jsonc")
 Tracker:AddLocations("locations/south/swamp.jsonc")
+Tracker:AddLocations("locations/south/junkyard_area.jsonc")
